b1960adbb15c2cf7514fc5322102e3b999b978c9
Gongas <goncalo.mesteves03@gmail.com>
2024-08-31 20:05:55 +0100

    Added a few notes

--- Server Client Multiplayer Game/app.js
+++ Server Client Multiplayer Game/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Make any file inside the public directory available to anybody
 app.use(express.static('public'))
 
+// Request to the Home Page
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html') // Send the index.html
 })
 
 app.listen(port, () => {
