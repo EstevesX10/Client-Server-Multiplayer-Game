111bb42eff8c6b94753a94eefdfc7259dc894dec
Gongas <goncalo.mesteves03@gmail.com>
2024-09-01 18:54:06 +0100

    Changed color on the frontend

--- Server Client Multiplayer Game/public/js/index.js
+++ Server Client Multiplayer Game/public/js/index.js
@@ -1,5 +1,3 @@
-import {Player} from "./classes/Player.js"
-
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
@@ -15,9 +13,6 @@ canvas.height = innerHeight
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-// Create a player
-const player = new Player(x, y, 10, 'white')
-
 // Define a frontend players object to render all the players onto the screen
 const frontendPlayers = {}
 
@@ -30,7 +25,7 @@ socket.on('updatePlayers', (backendPlayers) => {
 
     // If the current backend player does not exist on the frontend
     if(!frontendPlayers[id]){
-      frontendPlayers[id] = new Player(backendPlayer.x, backendPlayer.y, 10, 'white')
+      frontendPlayers[id] = new Player(backendPlayer.x, backendPlayer.y, 10, 'hsl(0, 100%, 50%)')
     }
   }
 
