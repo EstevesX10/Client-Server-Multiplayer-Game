d96bc3620400cc62f3b4ce8c296daf580ff7b7f5
Gongas <goncalo.mesteves03@gmail.com>
2024-09-01 18:26:16 +0100

    Added a updatePlayers feature to help broadcast the backend info to the frontend

--- Server Client Multiplayer Game/public/js/index.js
+++ Server Client Multiplayer Game/public/js/index.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+// Create a socket
+// -> When initialized it is attempting to create a connection with the backend server
+const socket = io()
+
 const scoreEl = document.querySelector('#scoreEl')
 
 canvas.width = innerWidth
@@ -9,7 +13,24 @@ canvas.height = innerHeight
 const x = canvas.width / 2
 const y = canvas.height / 2
 
+// Create a player
 const player = new Player(x, y, 10, 'white')
+
+// Define a frontend players object to render all the players onto the screen
+const players = {}
+
+// Receive the updatePlayers Event
+socket.on('updatePlayers', (backendPlayers) => {
+  // Iterate through all the backendPlayers
+  for (const id in backendPlayers){
+    const backendPlayer = backendPlayers[id]
+
+    // If the current backend player does not exist on the frontend
+    if(!players[id]){
+      players[id] = new Player(backendPlayer.x, backendPlayer.y, 10, 'white')
+    }
+  }
+})
 
 let animationId
 function animate() {
@@ -17,8 +38,11 @@ function animate() {
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
   
-  // Add player to the Screen
-  player.draw()
+  // Display all the Players
+  for (const id in players){
+    const player = players[id]
+    player.draw()
+  }
 }
 
 // Animation Loop
